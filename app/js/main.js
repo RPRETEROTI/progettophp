fdf59ee985236dc79a89b896680e3751bac28277
ROCCO <rocco.preteroti@amaris.com>
2020-12-27 17:21:54 +0100

    EDIT style

--- app/js/main.js
+++ app/js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 // functions related the scroll of navbar
 
 $(document).ready(function () {
-  if (document.documentElement.clientWidth >= 1279) {
+  if (document.documentElement.clientWidth > 1280) {
     $(window).scroll(function () {
       if ($(document).scrollTop() > 150) {
         $(".navigation-menu").css("height", "20%");
@@ -38,13 +38,13 @@ $(document).ready(function () {
   }
 });
 $(document).ready(function () {
-  if (document.documentElement.clientWidth < 1279) {
+  if (document.documentElement.clientWidth < 1280) {
     $(window).scroll(function () {
       if ($(document).scrollTop() > 150) {
-        $(".navigation-menu").css("height", "17%");
+        $(".navigation-menu").css("height", "18%");
         $(".navigation-menu ul").css("top", "40px");
       } else {
-        $(".navigation-menu").css("height", "16%");
+        $(".navigation-menu").css("height", "17%");
         $(".navigation-menu ul").css("top", "30px");
       }
     });
