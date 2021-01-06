1f64aa0040131cfe78d1c91cc9f7dbc125f55693
ROCCO <rocco.preteroti@amaris.com>
2021-01-06 19:49:13 +0100

    CREATE authentication api

---
+++ app/js/authenticate.js
@@ -0,0 +1,204 @@
+$(document).ready(function () {
+  // registrationTemplate();
+
+  // $(document).on("mouseover", ".login", function () {
+  //   $(this).addClass("backgr");
+  //   $(".signin :input").prop("disabled", true);
+  // });
+  // $(document).on("mouseout", ".login", function () {
+  //   $(this).removeClass("backgr");
+  //   $(".signin :input").prop("disabled", false);
+  // });
+  // $(document).on("mouseover", ".signin", function () {
+  //   $(this).addClass("backgr");
+  //   $(".login :input").prop("disabled", true);
+  // });
+  // $(document).on("mouseout", ".signin", function () {
+  //   $(this).removeClass("backgr");
+  //   $(".login :input").prop("disabled", false);
+  // });
+  registerTemplate();
+  $(document).on("click", ".signin", function () {
+    formTemplate();
+    $("#registerForm").find("button").text("SIGN IN");
+    $("#registerForm").find("button").attr("data-id", "signIn");
+    $("#registerForm").find("button").addClass("signbtn");
+    $(".formRegister").find(".title").text("Iscriviti alla piattaforma");
+  });
+  $(document).on("click", ".login", function () {
+    formTemplate();
+    $("#registerForm").find("button").text("LOGIN");
+    $("#registerForm").find("button").attr("data-id", "logIn");
+    $("#registerForm").find("button").addClass("loginbtn");
+    $(".formRegister").find(".title").text("Accedi alla piattaforma");
+  });
+
+  $(document).on("submit", "#registerForm", function (e) {
+    e.preventDefault();
+    var authenticate_id = $(this).find("button").attr("data-id");
+    var userName;
+    var passWord;
+    userName = $(this).find("input[name=usr]").val();
+    passWord = $(this).find("input[name=pwd]").val();
+    var form_data = JSON.stringify($(this).serializeObject()); // this = the submitted form
+    console.log("form_data", form_data);
+    console.log("key", userName, passWord);
+    console.log("cetrioli");
+    authenticate_id === "logIn" ? login(userName, passWord) : signin(form_data);
+  });
+});
+
+$.fn.serializeObject = function () {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function () {
+    if (o[this.name] !== undefined) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || "");
+    } else {
+      o[this.name] = this.value || "";
+    }
+  });
+  return o;
+};
+function login(usr, psw) {
+  $.ajax({
+    url:
+      "http://localhost/progetto_approcciavanzati2020/app/api/login.php?usr=" +
+      usr +
+      "&pwd=" +
+      psw,
+    method: "GET",
+    success: function (data) {
+      // successLogin(data);
+      location.href = "homepage.php";
+      console.log("dataaf", data);
+    },
+    error: function (xhr, err, exc) {
+      console.log("E stato riscontrato un errore", err);
+      console.log("E stato riscontrato un errore", xhr.responseText);
+      console.log("xhr", xhr);
+      console.log("E stato riscontrato un errore", exc);
+      var errorJsonMessage = JSON.parse(xhr.responseText);
+      failureLogin(errorJsonMessage);
+    },
+  });
+}
+
+function signin(form_data) {
+  $.ajax({
+    url: "http://localhost/progetto_approcciavanzati2020/app/api/signin.php",
+    type: "POST",
+    contentType: "application/json",
+    dataType: "json",
+    data: form_data,
+    success: function (result) {
+      console.log(result);
+      console.log(form_data);
+      location.href = "homepage.php";
+    },
+    error: function (xhr, err, exc) {
+      console.log("hai bsagliato:", xhr.responseText);
+    },
+  });
+  return false;
+}
+
+function successLogin() {
+  containerHtml = `
+  <div class="alert alert-primary" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+      SEistato loggato correttamente. Fottiti. 
+          </div>`;
+
+  // inject to 'page-content' of our app
+  $(".message").html(containerHtml);
+}
+{
+  /* <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div> */
+}
+function failureLogin(errorJsonMessage) {
+  $("#registerForm").find("input").val("");
+  containerHtml =
+    `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong> Retry. ` +
+    errorJsonMessage["message"] +
+    `
+    </strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">  <i class="fas fa-times"></i></span>
+  </button>
+    
+    </div>`;
+
+  // inject to 'page-content' of our app
+  $(".messageLogin").html(containerHtml);
+}
+
+function formTemplate() {
+  containerHtml = `	<div class="row formRegister">
+  <div class="col-12 ">
+    <h3 class="title"></h3>
+    <form action="#" method="post" id="registerForm" autocomplete="off">
+      <div class="form-row mypage">
+        <div class="form-group col-12 col-md-3 ">
+          <label for="usr" class="accesso col-form-label">Username</label>
+          <div>
+            <input name="usr" type="text" id="usr" class="credenziali form-control" />
+          </div>
+        </div>
+        <div class="form-group col-12 col-md-3 ">
+          <label for="usr" class="accesso col-form-label">Password</label>
+          <div>
+            <input name="pwd" type="password" id="pwd" class="credenziali form-control" />
+          </div>
+        </div>
+        <div class="col-12 col-md-2 containerBtn">
+          <button type="submit" data-id="" class="btn"></button>
+        </div>
+      </div>
+      <div class="form-row">
+      <div class="col-12 align-items-center d-flex justify-content-center messageLogin">
+      </div>
+    </div>
+      
+    </form>
+  </div>
+    </form>
+  </div>
+</div>`;
+
+  //   // inject to 'page-content' of our app
+  $(".registration").html(containerHtml);
+}
+{
+  /* <div class="form-row">
+        <div class="col-12 align-items-center d-flex justify-content-center">
+          <button type="submit" data-id="" class="btn"></button>
+        </div>
+      </div> */
+}
+function registerTemplate() {
+  containerHtml = `	
+
+        <div class="col-6 align-items-center d-flex justify-content-center login">
+          <button type="submit" class="btn loginbtn mypagebtn">LOGIN</button>
+        </div>
+        <div class="col-6 align-items-center d-flex justify-content-center signin">
+        <button type="submit" class="btn signbtn mypagebtn">SIGN IN</button>
+      </div>
+
+`;
+
+  // inject to 'page-content' of our app
+  $(".buttonregistration").html(containerHtml);
+}
--- app/js/login.js
+++ app/js/login.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  registrationTemplate();
+
+  $(document).on("mouseover", ".login", function () {
+    $(this).addClass("backgr");
+    $(".signin :input").prop("disabled", true);
+  });
+  $(document).on("mouseout", ".login", function () {
+    $(this).removeClass("backgr");
+    $(".signin :input").prop("disabled", false);
+  });
+  $(document).on("mouseover", ".signin", function () {
+    $(this).addClass("backgr");
+    $(".login :input").prop("disabled", true);
+  });
+  $(document).on("mouseout", ".signin", function () {
+    $(this).removeClass("backgr");
+    $(".login :input").prop("disabled", false);
+  });
   $(document).on("submit", "#loginForm", function (e) {
     e.preventDefault();
     var userName;
@@ -21,7 +39,8 @@ function login(usr, psw) {
       psw,
     method: "GET",
     success: function (data) {
-      successLogin(data);
+      // successLogin(data);
+      location.href = "homepage.php";
       console.log("dataaf", data);
     },
     error: function (xhr, err, exc) {
@@ -46,19 +65,90 @@ function successLogin() {
   // inject to 'page-content' of our app
   $(".message").html(containerHtml);
 }
-
+{
+  /* <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div> */
+}
 function failureLogin(errorJsonMessage) {
+  $("#loginForm").find("input").val("");
   containerHtml =
-    `<div class="alert alert-primary" role="alert">
-      C'è stato un problema. ` +
+    `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong> Retry. ` +
     errorJsonMessage["message"] +
     `
+    </strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">  <i class="fas fa-times"></i></span>
+  </button>
+    
     </div>`;
 
   // inject to 'page-content' of our app
-  $(".message").html(containerHtml);
+  $(".messageLogin").html(containerHtml);
 }
 
-//data-id="` +
-// val.dataId +
-// `">
+function registrationTemplate() {
+  containerHtml = `	<div class="row">
+  <div class="col-6 login">
+    <h3 class="title">Accedi al tuo profilo</h3>
+    <form action="#" method="post" id="loginForm" autocomplete="off">
+      <div class="form-row mypage">
+        <div class="form-group col-12 col-md-5 ">
+          <label for="usr" class="accesso col-form-label">Username</label>
+          <div>
+            <input name="usr" type="text" id="usr" class="credenziali form-control" />
+          </div>
+        </div>
+        <div class="form-group col-12 col-md-5 ">
+          <label for="usr" class="accesso col-form-label">Password</label>
+          <div>
+            <input name="pwd" type="password" id="pwd" class="credenziali form-control" />
+          </div>
+        </div>
+      </div>
+      <div class="form-row">
+      <div class="col-12 align-items-center d-flex justify-content-center messageLogin">
+      </div>
+    </div>
+      <div class="form-row">
+        <div class="col-12 align-items-center d-flex justify-content-center login">
+          <button type="submit"  class="btn mypagebtn">LOGIN</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="col-6 signin">
+    <h3 class="title">Crea il tuo profilo</h3>
+    <form action="#" method="post" id="loginForm"  autocomplete="off">
+      <div class="form-row mypage">
+        <div class="form-group col-12 col-md-5 ">
+          <label for="usr" class="accesso col-form-label">Username</label>
+          <div>
+            <input name="usr" type="text" id="usr" class="credenziali form-control" />
+            <small id="passwordHelpBlock" class="usrn message form-text"></small>
+          </div>
+        </div>
+        <div class="form-group col-12 col-md-5 ">
+          <label for="usr" class="accesso col-form-label">Password</label>
+          <div>
+            <input name="pwd" type="password" id="pwd" class="credenziali form-control" />
+            <small id="passwordHelpBlock" class="usrn message form-text"></small>
+          </div>
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="col-12 align-items-center d-flex justify-content-center">
+          <button type="submit" class="btn mypagebtn">SIGN IN</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>`;
+
+  // inject to 'page-content' of our app
+  $(".registration").html(containerHtml);
+}
